8b6cc83107f5df33d19de0489ec53bb2b2405d7c
Akarsh Srivastava <akrsh24@gmail.com>
2021-01-11 14:03:16 +0530

    Update store.js

--- react-with-redux/src/redux/store/store.js
+++ react-with-redux/src/redux/store/store.js
@@ -1,12 +1,15 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import logger from 'redux-logger'
-import reducer from "../reducers/reducer";
+import reducer from "../reducers/index";
 import thunk from "redux-thunk";
-import { composeWithDevTools } from 'redux-devtools-extension';
 
-const composeEnhancer = composeWithDevTools || compose
-const middleWare = [thunk, logger];
+const composeEnhancer =
+    (process.env.NODE_ENV !== 'production' &&
+        typeof window !== 'undefined' &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const middleWare = (process.env.NODE_ENV !== 'production' &&
+    typeof window !== 'undefined') ? [thunk, logger] : [thunk];
 
 export const store = createStore(
     reducer, composeEnhancer(applyMiddleware(...middleWare)));
-
